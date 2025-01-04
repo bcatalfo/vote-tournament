7df2b17ecd030862c6ba8a304d06c3d68e1d7d6b
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-04 16:20:25 -0500

    Changed the name and description from the defaults.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Vote Tournament" },
+    { name: "description", content: "Make your next decision democratically with Vote Tournament." },
   ];
 }
 
