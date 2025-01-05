d5ea3ed0865a62361307baa8302da8ccf19ad4aa
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-05 13:55:19 -0500

    Added an unordered list of band names.

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,7 +1,25 @@
 export function Welcome() {
+  const data = [
+    {
+      name: "Third Space",
+      id: 1,
+    },
+    {
+      name: "Rum Punches",
+      id: 2,
+    },
+  ];
+  const candidates = data.map((candiate) => (
+    <li key={candiate.id}>
+      <p>
+        <b>{candiate.name}</b>
+      </p>
+    </li>
+  ));
   return (
     <main>
       <h1>Vote Tournament</h1>
+      <ul>{candidates}</ul>
     </main>
   );
 }
