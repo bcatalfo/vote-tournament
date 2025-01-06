ed3cd11099e9e7d0598ae56dc5f2236cf595fb02
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-06 12:21:35 -0500

    Added more band names.

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -8,6 +8,42 @@ export function Welcome() {
       name: "Rum Punches",
       id: 2,
     },
+    {
+      name: "Sam and the Mumblos",
+      id: 3,
+    },
+    {
+      name: "Sam and the Martians",
+      id: 4,
+    },
+    {
+      name: "Mythical Island",
+      id: 5,
+    },
+    {
+      name: "Magical Journey",
+      id: 6,
+    },
+    {
+      name: "Common Area",
+      id: 7,
+    },
+    {
+      name: "The Martians",
+      id: 8,
+    },
+    {
+      name: "The Mumblos",
+      id: 9,
+    },
+    {
+      name: "Fruitless Pursuit",
+      id: 10,
+    },
+    {
+      name: "Genetic Apex: Charizard",
+      id: 11,
+    },
   ];
   const candidates = data.map((candiate) => (
     <li key={candiate.id}>
