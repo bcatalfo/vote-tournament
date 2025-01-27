24a2589945c2d659417d1aaac0cb231824c8b785
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-27 12:17:45 -0500

    Put the candidates in a centered flex column.

--- app/components/make-tournament.tsx
+++ app/components/make-tournament.tsx
@@ -5,11 +5,13 @@ export default function MakeTournament() {
   return (
     <div className="container mx-auto my-auto py-4 text-center">
       <h1>What to name our band?</h1>
-      <ul>
-        {bandNamesData.map((candidate) => (
-          <Candidate name={candidate.name} id={candidate.id.toString()} />
-        ))}
-      </ul>
+      <div className="flex flex-col items-center">
+        <ul>
+          {bandNamesData.map((candidate) => (
+            <Candidate name={candidate.name} id={candidate.id.toString()} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
