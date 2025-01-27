536244f9a5ec5f6655643af02568e747ec6e10af
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-27 15:51:07 -0500

    Added an input field to add elements to the list (just decorative rn).

--- app/components/make-tournament.tsx
+++ app/components/make-tournament.tsx
@@ -3,8 +3,16 @@ import { bandNamesData } from "./data";
 
 export default function MakeTournament() {
   return (
-    <div className="container mx-auto my-auto py-4 text-center">
-      <h1 className="text-xl p-8 font-bold">What to name our band?</h1>
+    <div className="container mx-auto my-auto text-center flex flex-col items-center">
+      <h1 className="text-xl p-4 font-bold">What to name our band?</h1>
+      <div className="w-1/4 flex py-4">
+        <input
+          type="text"
+          className="border-2 border-slate-200 rounded-md p-2 my-4 w-full"
+          placeholder="Type your suggestion here..."
+        />
+        <button className="p-2"> Add </button>
+      </div>
       <div className="flex flex-col items-center">
         <ul>
           {bandNamesData.map((candidate) => (
