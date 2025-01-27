0a21d98819da200e17434c186523a78a1216859c
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-27 15:12:03 -0500

    Added a bit of padding to the X and edit buttons for the candidate.

--- app/components/candidate.tsx
+++ app/components/candidate.tsx
@@ -3,9 +3,9 @@ export default function Candidate({ name, id }: { name: string; id: string }) {
     <div className="border rounded-sm p-4">
       <li key={id}>
         <p className="flex flex-row ">
-          <button> X </button>
+          <button className="p-1"> X </button>
           <b className="flex-grow p-4">{name}</b>
-          <button> Edit </button>
+          <button className="p-1"> Edit </button>
         </p>
       </li>
     </div>
