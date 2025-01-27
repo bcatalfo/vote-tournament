c2ce1f036f114d4ab3ecaf7b52ab49b2b68ee6b0
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-27 15:02:42 -0500

    Put the X before the band name and put it in a flex row.

--- app/components/candidate.tsx
+++ app/components/candidate.tsx
@@ -2,10 +2,10 @@ export default function Candidate({ name, id }: { name: string; id: string }) {
   return (
     <div className="border rounded-sm p-4">
       <li key={id}>
-        <p>
+        <p className="flex flex-row ">
+          <button> X </button>
           <b>{name}</b>
           <button> Edit </button>
-          <button> X </button>
         </p>
       </li>
     </div>
