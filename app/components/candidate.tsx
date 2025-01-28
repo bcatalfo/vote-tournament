50fcd74c8646b61f5314a454df268a8846fbe61d
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-28 18:01:58 -0500

    Decreased top and bottom padding for the candidate.

--- app/components/candidate.tsx
+++ app/components/candidate.tsx
@@ -6,12 +6,12 @@ export default function Candidate({
   deleteCallback: () => void;
 }) {
   return (
-    <div className="border-2 rounded-md p-4">
+    <div className="border-2 rounded-md px-4 py-2">
       <p className="flex flex-row ">
         <button className="p-1" onClick={deleteCallback}>
           X
         </button>
-        <b className="flex-grow p-4">{name}</b>
+        <b className="flex-grow px-4 py-2">{name}</b>
         <button className="p-1">Edit</button>
       </p>
     </div>
