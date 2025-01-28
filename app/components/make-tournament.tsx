679ad6df92accf38e95cf52b9a83d8422cb9343e
Benjamin Catalfo <bcatalfo@gmail.com>
2025-01-28 11:05:40 -0500

    Added an edit button for the title of the tournament.

--- app/components/make-tournament.tsx
+++ app/components/make-tournament.tsx
@@ -7,7 +7,10 @@ export default function MakeTournament() {
   var nextId = candidateData.length + 1;
   return (
     <div className="container mx-auto my-auto text-center flex flex-col items-center">
-      <h1 className="text-xl p-4 font-bold">What to name our band?</h1>
+      <div className="flex flex-row items-stretch">
+        <h1 className="text-xl p-4 font-bold">What to name our band?</h1>
+        <button className="p-1">Edit</button>
+      </div>
       <div className="w-1/4 flex py-4">
         <input
           type="text"
